e21d28dcb3f853f47f0015eabb46a8e3c56689d7
Sergey <bwg.it252@gmail.com>
2023-03-05 23:34:56 +0300

    [+] scan blocks

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { ScanBlocks } from './components/ScanBlocks';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <ScanBlocks/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
